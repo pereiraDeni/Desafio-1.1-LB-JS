0981a58de5eb15107c241bbee23eeb922378ec70
pereiraDeni <denilsonf.pereira@gmail.com>
2020-06-18 15:54:28 -0300

    Desafio 1

---
+++ index.js
@@ -0,0 +1,27 @@
+const name = "Carlos"
+const wight = 84
+const height = 1.88
+
+imc = (wight, height)  => Math.round((wight / Math.sqrt(height)))
+
+
+if (imc(wight,height) > 30) {
+    console.log(`${name} você está acima do peso`)
+}else console.log(`${name} você não está está acima do peso`)
+
+
+const name2 = 'Silvana'
+const gender = 'F'
+const age = 48
+const timeContribution = 23
+
+if(gender == 'F'){
+    if(((timeContribution+age) > 85) && (timeContribution > 30)) {
+      console.log(`${name} você pode se aposentar`)  
+    } else console.log(`${name2} você não pode se aposentar`)
+}
+if(gender == 'M') {
+    if(((timeContribution+age) > 95) && (timeContribution > 35)) {
+    console.log(`${name2} você pode se aposentar`)
+}else console.log(`${name2} você não pode se aposentar`)
+}
